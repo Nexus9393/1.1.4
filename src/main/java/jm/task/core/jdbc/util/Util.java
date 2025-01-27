252f9737d5a38409d28acf10f13f82d35a76fe0f
Pavel Skazhutin <skaazhutin@yandex.ru>
2025-01-27 17:25:49 +0900

    Initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,4 +1,9 @@
 package jm.task.core.jdbc.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import jm.task.core.jdbc.model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,6 +12,28 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/newbase";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "ljl;th";
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration()
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", USERNAME)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.hbm2ddl.auto", "update")
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
     public static Connection getConnection() {
         try {
